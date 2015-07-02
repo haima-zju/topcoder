d918b3e3810f9a28ceea52169120690d690a159a
shen chun <shenchun11235813@gmail.com>
2015-07-02 11:48:14 +0800

    add 144div1 and and problem Lottery

---
+++ 144div1/Lottery.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+using namespace std;
+
+const int maxCh = 100;
+const int maxBl = 8;
+class Lottery{
+public:
+  class Game{
+  public:
+    long long p;
+    string name;
+    Game(string str = ""): name(str){}
+  };
+  class Cmp{
+  public:
+    bool operator()(Game a, Game b){
+      if(a.p != b.p) return a.p < b.p;
+      else return a.name < b.name;
+    }
+  }myCmp;
+  vector<string> sortByOdds(vector<string> &rules){
+    vector<Game> gameList;
+    vector<vector<long long>> f1(maxCh+1, vector<long long>(maxCh+1, 0));
+    vector<vector<long long>> f2(maxCh+1, vector<long long>(maxCh+1, 0));
+    for(int i = 0; i <= maxCh; i++) f1[0][i] = f2[0][i] = 1;
+    for(int i = 1; i <= maxBl; i++){
+      for(int j = 1; j <= maxCh; j++){
+        for(int k = 1; k <= j; k++){
+          f1[i][j] += f1[i-1][k];
+        }
+        if(i == 1){
+          for(int k = 1; k <= j; k++){
+            f2[i][j] += f2[i-1][k];
+          }
+        }
+        else{
+          for(int k = 1; k < j; k++){
+            f2[i][j] += f2[i-1][k];
+          }
+        }
+      }
+    }
+    for(auto a : rules){
+      auto itr = a.find(':');
+      Game g(string(a.begin(), a.begin()+itr));
+      a.erase(a.begin(), a.begin()+itr+1);
+
+      stringstream ss;
+      int choices, blanks;
+      string sorted, unique;
+      ss << a;
+      ss >> choices >> blanks >> sorted >> unique;
+      if(sorted[0] == 'T' && unique[0] == 'T'){
+        g.p = f2[blanks][choices];
+      }
+      else if(sorted[0] == 'T' && unique[0] == 'F'){
+        g.p = f1[blanks][choices];
+      }
+      else if(sorted[0] == 'F' && unique[0] == 'T'){
+        g.p = 1;
+        for(int i = 0; i < blanks; i++){
+          g.p *= choices-i;
+        }
+      }
+      else{
+        g.p = 1;
+        while(blanks--) g.p *= choices;
+      }
+      gameList.push_back(g);
+    }
+    sort(gameList.begin(), gameList.end(), myCmp);
+    vector<string> ret;
+    for(auto e : gameList) ret.push_back(e.name);
+    return ret;
+  }
+};
